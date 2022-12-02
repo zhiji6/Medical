5e4439ce700db33baa97810f5d5760a31f44b25c
t5191558 <tian519lei@163.com>
2022-12-02 21:24:21 +0800

    ..

--- SZSI_Smart/Controllers/YBController.cs
+++ SZSI_Smart/Controllers/YBController.cs
@@ -17,13 +17,13 @@ namespace Client.Controllers
         [HttpGet]
         public ReturnData<string> Get(string guid)
         {
-            return new ReturnData<string>() { Data = "3423423894320" };
-            //string ybkh = "";//"%GAAFSAKSXSUKKWDKHDAD?;07734724145330238292?";
-            //if (ylzhlist.TryRemove(guid, out ybkh))
-            //{
-            //    return new ReturnData<string>() { Data = ybkh };
-            //}
-            //return new ReturnData<string>() { Data = ybkh };
+            //return new ReturnData<string>() { Data = "3423423894320" };
+            string ybkh = "";//"%GAAFSAKSXSUKKWDKHDAD?;07734724145330238292?";
+            if (ylzhlist.TryRemove(guid, out ybkh))
+            {
+                return new ReturnData<string>() { Data = ybkh };
+            }
+            return new ReturnData<string>() { Data = ybkh };
         }
 
         /// <summary>
